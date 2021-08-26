6601bb0eff840d1edae168820ee9687e594b66d0
matty <matismarinelli@gmail.com>
2021-08-26 16:00:49 -0300

    JS Slider

--- js/slider.js
+++ js/slider.js
@@ -6,3 +6,14 @@ const btnLeft = document.querySelector("btn-left");
 const btnRight = document.querySelector("btn-right");
 
 slider.insertAdjacenElement("afterbegin", sliderSectionLast);
+
+function Next() {
+    let sliderSectionFirst = documetn.querySelectorAll(".slider__section"[0]);
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s",
+    setTimeout(function() {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
